bb93d6e8907df76174caf6de4b2a6e97bde7e9f1
anchi84 <anarili@gmail.com>
2018-01-27 23:44:35 +0100

    done

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Flask APP."""
 
 from flask import Flask
-from flask import render_template
+from flask import request
 from flask import jsonify
 
 app = Flask(__name__)
@@ -11,7 +11,23 @@ def index():
     """Handle HTTP GET Request for the home route."""
     return "Echo"
 
-@app.route('/api/v1.0/echo/<term>', methods=['GET'])
-def echo(term):
+@app.route('/api/v1.0/echo', methods=['GET'])
+@app.route('/api/v1.0/echo/<msg>', methods=['GET'])
+def echo(msg=None):
     """Handle HTTP GET Request for the echo API endpoint."""
-    return jsonify({'msg': term})
+    data = {}
+    if msg:
+        data['msg'] = msg
+    elif 'msg' in request.args:
+        data['msg'] = request.args['msg']
+    else:
+        data['msg'] = "Nothing to echo ..."
+    return jsonify(data)
+
+@app.after_request
+def after_request(response):
+   """Tweak response."""
+   response.headers.add('Access-Control-Allow-Origin', '*')
+   # response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+   # response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+   return response
